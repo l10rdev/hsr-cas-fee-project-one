d372514181b0c790dc2d272131309030d6b2f177
p18750 <david.lichtsteiner@css.ch>
2020-04-24 19:03:42 +0200

    Add first Version of Theme Switcher

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,3 +4,12 @@ import {TaskService} from './services/task-service.js';
 document.addEventListener('DOMContentLoaded', TaskController.bootstrap({
     taskService: new TaskService(),
 }));
+
+//TODO Movo To Controller when Created
+document.querySelector('.header__them-switcher--light').addEventListener('click', () => {
+    document.documentElement.setAttribute('data-theme', 'dark');
+});
+
+document.querySelector('.header__them-switcher--dark').addEventListener('click', () => {
+    document.documentElement.removeAttribute('data-theme');
+});
