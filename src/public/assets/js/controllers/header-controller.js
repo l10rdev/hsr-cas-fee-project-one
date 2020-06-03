5e61fd1f24c9c8bfc50ce349c37ecf5fc479d984
p18750 <david.lichtsteiner@css.ch>
2020-06-03 20:03:39 +0200

    Set theme at page setup

--- src/public/assets/js/controllers/header-controller.js
+++ src/public/assets/js/controllers/header-controller.js
@@ -6,21 +6,15 @@ export default class HeaderController {
 
   addEventListeners() {
     this.document.querySelector('.header__them-switcher--light').addEventListener('click', () => {
-      document.documentElement.setAttribute('data-theme', 'dark');
       this.themeService.set('dark');
     });
 
     this.document.querySelector('.header__them-switcher--dark').addEventListener('click', () => {
-      document.documentElement.removeAttribute('data-theme');
       this.themeService.set('light');
     });
   }
 
   init() {
-    const theme = this.themeService.get();
-    if (theme === 'dark') {
-      document.documentElement.setAttribute('data-theme', 'dark');
-    }
     this.addEventListeners();
   }
 
--- src/public/assets/js/services/theme-service.js
+++ src/public/assets/js/services/theme-service.js
@@ -1,9 +1,16 @@
 export default class ThemeService {
   constructor(service) {
     this.service = service;
+    this.set(this.service.get('theme'));
   }
 
   set(theme) {
+    if (theme === 'dark') {
+      document.documentElement.setAttribute('data-theme', 'dark');
+    } else {
+      document.documentElement.removeAttribute('data-theme');
+    }
+
     this.service.set('theme', theme);
   }
 
